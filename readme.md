460288f3074f768a31032523dd8548eaa820b1da
setsal <langeniustw@gmail.com>
2019-10-02 00:46:07 +0800

    first commit & add simple crud user method

---
+++ readme.md
@@ -0,0 +1,6 @@
+## Laravel OpenVPN Admin
+
+An admin panel written in laravel for openvpn management
+
+## Contributor
++ setsal Lan
---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div class="col-md-3">
+            <div class="card">
+                <div class="card-header">
+                    功能表
+                </div>
+
+                <div class="card-body">
+                    <ul class="nav flex-column" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link" href="">
+                                    123
+                                </a>
+                            </li>
+                    </ul>
+                </div>
+            </div>
+            <br/>
+</div>
